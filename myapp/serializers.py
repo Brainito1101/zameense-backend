4b30f8f1544d503b66e31248464b8b45115d9bb4
asha <your_email@example.com>
2026-04-16 15:07:30 +0530

    update serializer and view

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -3,17 +3,6 @@ from .models import Land, Lead, LandImage, SavedProperty, Inquiry
 
 
 class LandImageSerializer(serializers.ModelSerializer):
-    land = serializers.PrimaryKeyRelatedField(
-        queryset=Land.objects.all()
-    )
-
-    def to_internal_value(self, data):
-        # Convert land to integer if it's a string
-        if 'land' in data and isinstance(data['land'], str):
-            data = data.copy()
-            data['land'] = int(data['land'])
-        return super().to_internal_value(data)
-
     class Meta:
         model = LandImage
         fields = '__all__'
